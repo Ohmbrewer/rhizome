9fa03c4845c2091bc0d7aa63b9cbc3fa2ab2ca14
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-09-22 21:19:04 -0700

    Fixes some documentation regarding some Temperature instances.

--- lib/Ohmbrewer_Temperature_Sensor.h
+++ lib/Ohmbrewer_Temperature_Sensor.h
@@ -34,7 +34,7 @@ namespace Ohmbrewer {
 
             /**
              * The last temperature read by the sensor. Currently returns in Celsius.
-             * @returns The last temperature reading
+             * @returns A pointer to the Temperature object representing the last temperature reading
              */
             Temperature* getTemp() const;
 
@@ -53,24 +53,24 @@ namespace Ohmbrewer {
 
             /**
              * Constructor
-             * @param id The Sprout ID to use for this piece of Equipment
-             * @param pins The list of physical pins this Equipment is attached to
+             * @param id The Sprout ID to use for this TemperatureSensor
+             * @param pins The list of physical pins this TemperatureSensor is attached to
              */
             TemperatureSensor(int id, std::list<int>* pins);
 
             /**
              * Constructor
-             * @param id The Sprout ID to use for this piece of Equipment
-             * @param pins The list of physical pins this Equipment is attached to
-             * @param stopTime The time at which the Equipment should shut off, assuming it isn't otherwise interrupted
-             * @param state Whether the Equipment is ON (or OFF). True => ON, False => OFF
-             * @param currentTask The unique identifier of the task that the Equipment believes it should be processing
+             * @param id The Sprout ID to use for this TemperatureSensor
+             * @param pins The list of physical pins this TemperatureSensor is attached to
+             * @param stopTime The time at which the TemperatureSensor should shut off, assuming it isn't otherwise interrupted
+             * @param state Whether the TemperatureSensor is ON (or OFF). True => ON, False => OFF
+             * @param currentTask The unique identifier of the task that the TemperatureSensor believes it should be processing
              */
             TemperatureSensor(int id, std::list<int>* pins, int stopTime, bool state, String currentTask);
 
             /**
              * Copy Constructor
-             * @param clonee The Equipment object to copy
+             * @param clonee The TemperatureSensor object to copy
              */
             TemperatureSensor(const TemperatureSensor& clonee);
             
@@ -85,8 +85,8 @@ namespace Ohmbrewer {
             // friend std::ostream& operator<<( std::ostream& os, Pump const& pump);
 
             /**
-             * Specifies the interface for arguments sent to this Equipment's associated function.
-             * Parses the supplied string into an array of strings for setting the Equipment's values.
+             * Specifies the interface for arguments sent to this TemperatureSensor's associated function.
+             * Parses the supplied string into an array of strings for setting the TemperatureSensor's values.
              * Most likely will be called during update().
              * @param argsStr The arguments supplied as an update to the Rhizome.
              * @param result A map representing the key/value pairs for the update
@@ -94,32 +94,32 @@ namespace Ohmbrewer {
             static void parseArgs(const String &argsStr, args_map_t &result);
 
             /**
-             * Sets the Equipment state. True => On, False => Off
-             * @param state Whether the Equipment is ON (or OFF). True => ON, False => OFF
+             * Sets the TemperatureSensor state. True => On, False => Off
+             * @param state Whether the TemperatureSensor is ON (or OFF). True => ON, False => OFF
              * @returns The time taken to run the method
              */
             const int setState(const bool);
 
             /**
-             * The Equipment state. True => On, False => Off
+             * The TemperatureSensor state. True => On, False => Off
              * @returns True => On, False => Off
              */
             bool getState() const;
 
             /**
-             * True if the Equipment state is On.
+             * True if the TemperatureSensor state is On.
              * @returns Whether the Equipment is turned ON
              */
             bool isOn() const;
 
             /**
-             * True if the Equipment state is Off.
+             * True if the TemperatureSensor state is Off.
              * @returns Whether the Equipment is turned OFF
              */
             bool isOff() const;
 
             /**
-             * Performs the Equipment's current task. Expect to use this during loop().
+             * Performs the TemperatureSensor's current task. Expect to use this during loop().
              * This function is called by work().
              * @returns The time taken to run the method
              */
@@ -145,7 +145,7 @@ namespace Ohmbrewer {
             /**
              * Reports which of the Rhizome's pins are occupied by the
              * Equipment, forming a logical Sprout.
-             * @returns The list of physical pins that the Equipment is connected to.
+             * @returns The list of physical pins that the TemperatureSensor is connected to.
              */
             std::list<int>* whichPins() const;
 
@@ -154,6 +154,7 @@ namespace Ohmbrewer {
              * Last temperature read by the sensor
              */
             Temperature* _lastReading;
+
             /**
              * Last time the temperature was read by the sensor
              */
