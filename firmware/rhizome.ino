ecee8f6f5e0882ee8c91165a720ab995c03182f1
azyth <azyth@carbon>
2015-10-31 19:07:23 -0400

    PID functionality

--- firmware/rhizome.ino
+++ firmware/rhizome.ino
@@ -7,14 +7,15 @@
 #include "Ohmbrewer_Publisher.h"
 #include "Ohmbrewer_Thermostat.h"
 #include "Ohmbrewer_RIMS.h"
+#include "Ohmbrewer_Onewire.h"
 #include "onewire.h"
-// #include "Ohmbrewer_Publisher.h" // (*)
 
 // Kludge to allow us to use std::list - for now we have to undefine these macros.
 #undef min
 #undef max
 #undef swap
 #include <deque>
+#include <list>
 
 /* ========================================================================= */
 /*  Global Vars                                                              */
@@ -31,6 +32,9 @@ std::deque< Ohmbrewer::Equipment* > sprouts;
  * update it within the Sprout management functions.
  */
 int inUse[6] = {0,0,0,0,0,0};
+
+//initializer Pin list
+std::list<int> thermPins (1, 0);
 
 /**
  * The touchscreen object. Handles the display for the Rhizome.
@@ -51,7 +55,7 @@ unsigned long lastUpdate = millis();
  * Does any preliminary setup work before the Rhizome starts the operation loop.
  */
 void setup() {
-    // Serial.begin(9600); // Enable serial for debugging messages
+//    Serial.begin(9600); // Enable serial for debugging messages
     String fakeTask = "fake";
 
     ow_setPin(D0); //This should later be accomplished by equipment setup OR constructor
@@ -59,37 +63,34 @@ void setup() {
     // Add our initial Equipment. We wouldn't necessarily do this, but it's useful for now.
     // We'll also set some temperatures and relay values explicitly when we probably wouldn't.
 // EX 1: Temperature Sensors
-//    sprouts.push_back(new Ohmbrewer::TemperatureSensor( 1, new std::deque<int>(1,0) ));
+//    sprouts.push_back(new Ohmbrewer::TemperatureSensor( 1, new std::list<int>(1,0) ));
 //    ((Ohmbrewer::TemperatureSensor*)sprouts.back())->getTemp()->set(42);
 //
-//    sprouts.push_back(new Ohmbrewer::TemperatureSensor( 2, new std::list<int>(1,1) ));
+//    sprouts.push_back(new Ohmbrewer::TemperatureSensor( 2, new std::list<int>(1,0) ));
 //    ((Ohmbrewer::TemperatureSensor*)sprouts.back())->getTemp()->set(-20);
 
 // EX 2: A Thermostat
-//    sprouts.push_back(new Ohmbrewer::Thermostat( 1, new std::list<int>(1,0), 100 ));
+//    sprouts.push_back(new Ohmbrewer::Thermostat( 1, new std::list<int>(3,0), 100 ));
 //    ((Ohmbrewer::Thermostat*)sprouts.front())->getElement()->setState(true);
 
 // EX 3: Pumps & Heating Elements
 //    sprouts.push_back(new Ohmbrewer::Pump( 1, new std::list<int>(1,2), 0, true, fakeTask ));
 //    sprouts.push_back(new Ohmbrewer::Pump( 2, new std::list<int>(1,3) ));
-//    sprouts.push_back(new Ohmbrewer::HeatingElement( 1, new std::list<int>(1,4) ));
-//    sprouts.push_back(new Ohmbrewer::HeatingElement( 2, new std::list<int>(1,5), 0, true, fakeTask ));
+//    sprouts.push_back(new Ohmbrewer::HeatingElement( 3, new std::list<int>(1,4) ));
+//    sprouts.push_back(new Ohmbrewer::HeatingElement( 4, new std::list<int>(1,5), 0, true, fakeTask ));
 
 // EX 4: A RIMS w/ a Pump to the Kettle
-    //TODO change to array
-    // [ tube temp busPin ; tube heating powerPin ; tube heating controlPin ; mashtun temp busPin ; pump powerPin ; pump controlPin]
-
-    int tubePins[] = {0,1,2};
-    int tunPin = 3;
-    int pumpPins[] = {4,5};
-    //for(int i = 1; i < 5; i++) {rimsPins->push_back(i);}
-
-    sprouts.push_back(new Ohmbrewer::RIMS( 1, tubePins, tunPin, pumpPins ));
+    thermPins.push_back(2); //control pin (relay pin)
+    thermPins.push_back(3); //power pin (switch pin)
+    int pumpPin = 4;
+    int pumpPin2 = 5;
+    sprouts.push_back(new Ohmbrewer::RIMS( 1, &thermPins, pumpPin ));
+    sprouts.push_back(new Ohmbrewer::Pump( 8 , pumpPin2 )); //transfer pump
     ((Ohmbrewer::RIMS*)sprouts.front())->setState(true); // Turn everything on.
-    ((Ohmbrewer::RIMS*)sprouts.front())->getTube()->setTargetTemp(559);
+    ((Ohmbrewer::Thermostat*)sprouts.front())->setState(true); // Turn everything on.
+    ((Ohmbrewer::RIMS*)sprouts.front())->getTube()->setTargetTemp(62.7);
 
-    sprouts.push_back(new Ohmbrewer::Pump( 2, pumpPins[0], pumpPins[1] ));
-
+//TURN ON screen
     screen.initScreen();
 //    pMap[String("hey")] = String("listen!"); // (*)
 }
@@ -98,19 +99,23 @@ void setup() {
  * The meat of the program. Runs repeatedly until the Rhizome is powered off.
  */
 void loop() {
+    //probe ID finder command (switch this for the other below commands to enable)
+//    ((Ohmbrewer::Onewire*)(((Ohmbrewer::RIMS*)sprouts.front())->getTube()->getSensor()->getProbe()))
+//            ->displayProbeIds(&screen);
+
+
+
     if((millis() - lastUpdate) > 10000) {
         // Toggle the last relay every 10 seconds, for illustration.
-//        sprouts.back()->setState(sprouts.back()->isOff()); // The last HeatingElement
-//        ((Ohmbrewer::Thermostat*)sprouts.at(0))->getElement()
-//                                               ->setState(!((Ohmbrewer::Thermostat*)sprouts.at(0))->getElement()
-//                                                                                                  ->getState()); // The Thermostat's HeatingElement
         ((Ohmbrewer::RIMS*)sprouts.front())->getTube()
                                            ->getElement()
                                            ->toggleState();
-//        navi->publish(); // (*)
+
         lastUpdate = millis();
     }
-    ((Ohmbrewer::RIMS*)sprouts.front())->getTube()->getSensor()->doWork(); //Temp patch to make sensor read in TUBE
+    ((Ohmbrewer::RIMS*)sprouts.front())->getTube()->getSensor()->work(); //Temp patch to make sensor read in Tun temp
+
+    //refresh the display
     screen.refreshDisplay();
 }
 
